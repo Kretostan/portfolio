388964f4e22f5778499d0a6530f08cd2300fbb27
Kretostan <kubakret82@icloud.com>
2025-08-13 16:02:18 +0200

    refactor: remove unnecessary code

--- vite.config.ts
+++ vite.config.ts
@@ -8,13 +8,4 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-  server: {
-    proxy: {
-      "/api": {
-        target: 'http://backend:3000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      }
-    }
-  }
 })
