57c62428208f0831cec8362214804f396e274860
Kretostan <kubakret82@icloud.com>
2025-08-14 03:24:37 +0200

    feat: add loading spinner component

--- src/components/ui/Spinner.tsx
+++ src/components/ui/Spinner.tsx
@@ -1,8 +1,10 @@
 const Spinner = () => {
-  return <div className="text-center">
-    <div className="spinner"></div>
-    <p>Loading projects...</p>
-  </div>
-}
+  return (
+    <div className="py-12 text-center">
+      <div className="spinner"></div>
+      <p>Loading projects...</p>
+    </div>
+  );
+};
 
 export default Spinner;
