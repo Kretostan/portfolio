b1c9e20caaee9a4cf701b4e7bf7e92284da222d5
Kretostan <kubakret82@icloud.com>
2025-08-14 02:47:10 +0200

    refactor: restructure imports and update router configuration
    - Moved loaders directly into destructured imports
    - Added new routes: /auth/login and /auth/admin
    - Fixed the route /projects/update/:projectSlug (added parameter)
    - Updated import formatting and added missing commas

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
 import { RouterProvider, createBrowserRouter } from "react-router";
 
 import RootLayout from "./pages/Root.tsx";
-import ErrorPage from "./pages/Error.tsx"
-import HomePage from "./pages/Home.tsx"
+import ErrorPage from "./pages/Error.tsx";
+import HomePage from "./pages/Home.tsx";
 import AboutPage from "./pages/About.tsx";
 import SkillsPage from "./pages/Skills.tsx";
-import ProjectsPage from "./pages/projects/Projects.tsx";
+import ProjectsPage, {
+  loader as projectsLoader,
+} from "./pages/projects/Projects.tsx";
 import CreatorPage from "./pages/projects/Creator.tsx";
-import ProjectPage from "./pages/projects/Project.tsx";
+import ProjectPage, {
+  loader as projectLoader,
+} from "./pages/projects/Project.tsx";
 import ContactPage from "./pages/Contact.tsx";
 import LoginPage from "./pages/Login.tsx";
-import UpdaterPage from "./pages/projects/Updater.tsx";
-
-import { loader as projectsLoader } from "./pages/projects/Projects.tsx";
-import { loader as projectLoader } from "./pages/projects/Project.tsx";
+import UpdaterPage, {
+  loader as updaterLoader,
+} from "./pages/projects/Updater.tsx";
+import AdminPage from "./pages/Admin.tsx";
 
 const router = createBrowserRouter([
   {
@@ -25,16 +29,26 @@ const router = createBrowserRouter([
       { path: "about", element: <AboutPage /> },
       { path: "skills", element: <SkillsPage /> },
       { path: "projects", element: <ProjectsPage />, loader: projectsLoader },
-      // TODO
-      { path: "projects/:projectSlug", element: <ProjectPage />, loader: ({params}) => projectLoader({params}) },
+      {
+        path: "projects/:projectSlug",
+        element: <ProjectPage />,
+        // TODO
+        loader: ({ params }) => projectLoader({ params }),
+      },
       { path: "projects/add", element: <CreatorPage /> },
-      { path: "projects/update", element: <UpdaterPage /> },
+      {
+        path: "projects/update/:projectSlug",
+        element: <UpdaterPage />,
+        // TODO
+        loader: ({ params }) => updaterLoader({ params }),
+      },
       { path: "contact", element: <ContactPage /> },
-      { path: "login", element: <LoginPage /> }
-    ]
+      { path: "auth/login", element: <LoginPage /> },
+      { path: "auth/admin", element: <AdminPage /> },
+    ],
   },
 ]);
 
 export default function App() {
-  return <RouterProvider router={router} />
+  return <RouterProvider router={router} />;
 }
