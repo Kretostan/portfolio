01c022fe5f069c0b9e5d86cf39e618a3fc9da94f
Kretostan <kubakret82@icloud.com>
2025-08-14 19:44:17 +0200

    feat: add error message upon putting invalid values

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,13 +1,19 @@
-import axios from "axios";
+import { type FormEvent, useState } from "react";
 import { useDispatch } from "react-redux";
-
-import type { FormEvent } from "react";
+import axios, { type AxiosResponse } from "axios";
 
 import { login } from "../store/authSlice.ts";
 import Button from "../components/ui/Button.tsx";
 import Title from "../components/ui/Title.tsx";
 
+type loginResponse = {
+  token: string;
+  role: string;
+  success: boolean;
+};
+
 const LoginPage = () => {
+  const [error, setError] = useState<boolean>(false);
   const dispatch = useDispatch();
 
   const handleLogin = async (event: FormEvent<HTMLFormElement>) => {
@@ -16,34 +22,42 @@ const LoginPage = () => {
     const form = event.target as HTMLFormElement;
     const formData = new FormData(form);
     const jsonData = Object.fromEntries(formData.entries());
-    const response = await axios.post(
+    const response: AxiosResponse<loginResponse> = await axios.post(
       import.meta.env.VITE_API_URL + "/login",
       jsonData,
       {
         withCredentials: true,
       },
     );
-    if (response.status === 200) {
+
+    if (response.data.success) {
       dispatch(login({ token: response.data.token, role: response.data.role }));
-      window.location.href = "/";
+      return (window.location.href = "/");
     }
+    setError(true);
   };
 
   return (
     <>
       <Title>Login Page</Title>
       <div className="flex flex-col justify-center items-center p-6 bg-bg-theme-2 rounded-lg ring-1">
-        <form onSubmit={handleLogin} className="flex flex-col gap-4 w-[250px]">
-          <div className="flex flex-col">
-            <label htmlFor="email">E-mail:</label>
-            <input type="email" name="email" placeholder="E-mail" />
+        <form onSubmit={handleLogin} className="flex flex-col gap-3 w-[250px]">
+          <div className="flex flex-col gap-4">
+            <div className="flex flex-col">
+              <label htmlFor="email">E-mail:</label>
+              <input type="email" name="email" placeholder="E-mail" />
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="password">Password:</label>
+              <input type="password" name="password" placeholder="Password" />
+            </div>
           </div>
-          <div className="flex flex-col">
-            <label htmlFor="password">Password:</label>
-            <input type="password" name="password" placeholder="Password" />
-          </div>
-          {/*TODO: Jakiś error walidacji*/}
-          <div className="flex items-center justify-center pt-6">
+          <p
+            className={`${error ? "visible" : "invisible"} text-[var(--error-color)] text-sm`}
+          >
+            Email or password incorrect
+          </p>
+          <div className="flex items-center justify-center pt-4">
             <Button>Login</Button>
           </div>
         </form>
