8df1ffd9a6a4ba29e0956122fb5bdca53fe76fe0
Kretostan <kubakret82@icloud.com>
2025-08-15 05:54:29 +0200

    styles: change button background color and remove border

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -2,15 +2,17 @@ import { motion } from "framer-motion";
 import { useTheme } from "next-themes";
 
 const Button = ({ children }: { children: string }) => {
-    const { resolvedTheme } = useTheme();
+  const { resolvedTheme } = useTheme();
 
-    return <motion.button
-        className="px-6 py-2 border-1 border-none text-[#fff] rounded-xl cursor-pointer bg-accent-theme-1"
-        whileHover={{ opacity: 0.85, backgroundColor: "var(--accent-color-2)" }}
-        key={resolvedTheme}
+  return (
+    <motion.button
+      className="px-6 py-2 bg-accent-theme-2 text-white rounded-lg cursor-pointer"
+      whileHover={{ opacity: 0.85, backgroundColor: "var(--accent-color-1)" }}
+      key={resolvedTheme}
     >
-        {children}
+      {children}
     </motion.button>
-}
+  );
+};
 
-export default Button
+export default Button;
