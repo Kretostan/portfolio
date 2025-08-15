6382107b7bd9271642e884bb719648e4b91dffa7
Kretostan <kubakret82@icloud.com>
2025-08-15 06:40:44 +0200

    fix: reformat code and change text below title

--- src/pages/projects/Projects.tsx
+++ src/pages/projects/Projects.tsx
@@ -1,28 +1,20 @@
 import {Suspense} from "react";
-import {Await, useLoaderData, useNavigate} from "react-router";
-import {useSelector} from "react-redux";
+import {Await, useLoaderData} from "react-router";
 
 import Title from '../../components/ui/Title';
 import Projects from "../../components/Projects/Projects";
 import Spinner from "../../components/ui/Spinner";
 
-import type {RootState} from "../../store/store.ts";
-
 const ProjectsPage = () => {
-	const {projects} = useLoaderData();
-	const auth = useSelector((state: RootState) => state.auth);
-	const navigate = useNavigate();
+	const { projects } = useLoaderData();
 
 	return <>
-		<Title>Projects page</Title>
-		<p className="text-center">
-			To jest lista projektów. Wybierz jakiś po więcej informacji.
-		</p>
+		<Title>My Projects</Title>
+		<p className="text-center">Check out my work - click a project to see the details.</p>
 		<div className="flex flex-col justify-center items-center gap-6">
-			{auth.role === "admin" && <button onClick={() => navigate("add")} className="bg-accent-theme-1 text-bg-theme-2 px-4 py-2 rounded-lg">Add Project</button>}
 			<Suspense fallback={<Spinner />}>
 				<Await resolve={projects}>
-					{(resolvedProjects) => <Projects projects={resolvedProjects} />}
+					{(resolvedProjects) => <Projects data={resolvedProjects} />}
 				</Await>
 			</Suspense>
 		</div>
@@ -32,12 +24,11 @@ const ProjectsPage = () => {
 export default ProjectsPage;
 
 const loadProjects = async () => {
-	const response = await fetch(import.meta.env.VITE_API_URL + "/projects");
+	const response: Response = await fetch(import.meta.env.VITE_API_URL + "/projects");
 	if (!response.ok) {
-		throw new Response("Failed to load projects", {status: response.status, statusText: response.statusText});
+		throw new Response("Failed to load projects");
 	}
-	const data = await response.json();
-	return data.projects;
+	return await response.json();
 }
 
 export const loader = () => {
