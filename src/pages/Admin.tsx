1080204717c81ad59bdfd099fe7265f83814aa06
Kretostan <kubakret82@icloud.com>
2025-08-15 05:49:50 +0200

    feat: add admin page with add project functionality

---
+++ src/pages/Admin.tsx
@@ -0,0 +1,34 @@
+import { useSelector } from "react-redux";
+import { Navigate, useNavigate } from "react-router";
+
+import Title from "../components/ui/Title.tsx";
+
+import type { RootState } from "../store/store.ts";
+
+const AdminPage = () => {
+  const auth = useSelector((state: RootState) => state.auth);
+  const navigate = useNavigate();
+
+  if (!auth.token && auth.role !== "admin") {
+    return <Navigate to="/" replace />;
+  }
+
+  return (
+    <div>
+      <Title>Admin Panel</Title>
+      <div>
+        <h2>Projekty</h2>
+        <div>
+          <button
+            onClick={() => navigate("/projects/add")}
+            className="bg-accent-theme-1 text-bg-theme-2 px-4 py-2 rounded-lg coursor-pointer"
+          >
+            Add Project
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AdminPage;
