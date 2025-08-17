67452b2307b9f5fd73aaecb653d145b2efb4cc4a
Kretostan <kubakret82@icloud.com>
2025-08-17 23:57:32 +0200

    style: add modal animation

--- src/components/UI/Modal.tsx
+++ src/components/UI/Modal.tsx
@@ -1,5 +1,6 @@
 import { type ReactNode, useRef } from "react";
 import { createPortal } from "react-dom";
+import { motion } from "framer-motion";
 
 type ModalType = {
   children: ReactNode;
@@ -16,13 +17,24 @@ const Modal = ({ children, open, onClose }: ModalType) => {
   if (!modalRoot) return null;
 
   return createPortal(
-    <dialog
-      ref={dialogRef}
-      onClose={onClose}
-      className="absolute top-1/2 left-1/2 -translate-1/2 flex flex-col items-center justify-center gap-10 py-6 px-10 border-1 rounded-lg"
-    >
-      {children}
-    </dialog>,
+    <>
+      <div className="fixed inset-0 bg-black/50 z-40"></div>
+      <motion.dialog
+        ref={dialogRef}
+        onClose={onClose}
+        initial={{
+          y: 100,
+          opacity: 0,
+        }}
+        animate={{
+          y: 0,
+          opacity: 1,
+        }}
+        className="absolute top-1/2 left-1/2 -translate-1/2 flex flex-col items-center justify-center gap-10 py-6 px-10 border-1 rounded-lg z-50"
+      >
+        {children}
+      </motion.dialog>
+    </>,
     modalRoot,
   );
 };
