8d509cd05cc3bb385e6f977f7333ad8b9bed4ecb
Kretostan <kubakret82@icloud.com>
2025-08-19 17:00:50 +0200

    refactor: refactor code and change import after change of types directory

--- src/components/Projects/Project.tsx
+++ src/components/Projects/Project.tsx
@@ -1,22 +1,29 @@
-import type {Project} from "../../types";
+import type { Project } from "../../@types";
 
-const ProjectContent = ({project}: {project: Project}) => {
-
-  return <div className="flex flex-col justify-center items-center bg-bg-theme-2 rounded-xl">
-    <img
-      src={project.image}
-      alt={`${project.title} home page`}
-      className="rounded-lg m-4 project-image max-w-[750px]"
-    />
-    <div className="flex flex-col justify-center items-center gap-10 py-8 w-full">
-      <h2 className="text-xl sm:text-2xl lg:text-3xl font-semibold">{project.title}</h2>
-      <p>{project.description}</p>
-      <div className="flex w-full justify-around">
-        <a href={project.url} target="_blank" rel="noopener noreferrer">Visit Page</a>
-        <a href={project.github} target="_blank" rel="noopener noreferrer">Explore Repo</a>
+const ProjectContent = ({ project }: { project: Project }) => {
+  return (
+    <div className="flex flex-col justify-center items-center bg-bg-theme-2 rounded-xl">
+      <img
+        src={project.image}
+        alt={`${project.title} home page`}
+        className="rounded-lg m-4 project-image max-w-[750px]"
+      />
+      <div className="flex flex-col justify-center items-center gap-10 py-8 w-full">
+        <h2 className="text-xl sm:text-2xl lg:text-3xl font-semibold">
+          {project.title}
+        </h2>
+        <p>{project.description}</p>
+        <div className="flex w-full justify-around">
+          <a href={project.url} target="_blank" rel="noopener noreferrer">
+            Visit Page
+          </a>
+          <a href={project.github} target="_blank" rel="noopener noreferrer">
+            Explore Repo
+          </a>
+        </div>
       </div>
     </div>
-  </div>
-}
+  );
+};
 
 export default ProjectContent;
