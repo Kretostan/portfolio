8e5637390389790bb879d48cd2e9f994099659b8
Kretostan <kubakret82@icloud.com>
2025-08-19 06:03:07 +0200

    style: change backdrop bacgrkound color, dialog animation and padding

--- src/components/UI/Modal.tsx
+++ src/components/UI/Modal.tsx
@@ -18,19 +18,19 @@ const Modal = ({ children, open, onClose }: ModalType) => {
 
   return createPortal(
     <>
-      <div className="fixed inset-0 bg-black/50 z-40"></div>
+      <div className="fixed inset-0 bg-black/65 z-40"></div>
       <motion.dialog
         ref={dialogRef}
         onClose={onClose}
         initial={{
-          y: 100,
+          scale: 0,
           opacity: 0,
         }}
         animate={{
-          y: 0,
+          scale: 1,
           opacity: 1,
         }}
-        className="absolute top-1/2 left-1/2 -translate-1/2 flex flex-col items-center justify-center gap-10 py-6 px-10 border-1 rounded-lg z-50"
+        className="absolute top-1/2 left-1/2 -translate-1/2 flex flex-col items-center justify-center gap-8 py-8 px-12 border-1 border-accent-theme-2 rounded-lg z-50"
       >
         {children}
       </motion.dialog>
