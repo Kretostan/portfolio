7d15ad7d466af6d32d105913bb5a60cc11c9c8c9
Kretostan <kubakret82@icloud.com>
2025-08-20 17:01:27 +0200

    refactor: change imports path

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,8 +1,8 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
-import enNs1 from "../locales/en/en.json";
-import plNs1 from "../locales/pl/pl.json";
+import enNs1 from "../locales/en.json";
+import plNs1 from "../locales/pl.json";
 
 i18n.use(initReactI18next).init({
   debug: true,
@@ -10,7 +10,7 @@ i18n.use(initReactI18next).init({
     en: { ns1: enNs1 },
     pl: { ns1: plNs1 },
   },
-  fallbackLng: "en",
+  fallbackLng: ["en", "pl"],
   defaultNS: "ns1",
   ns: ["ns1"],
 });
