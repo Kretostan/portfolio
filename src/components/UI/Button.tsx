88d7b72b182cf324a607302a3829217a327925c5
Kretostan <kubakret82@icloud.com>
2025-08-21 05:14:29 +0200

    feat: extend Button component with custom className

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -4,14 +4,18 @@ import { useTheme } from "next-themes";
 type ButtonType = {
   children: string;
   onClick?: () => void;
+  className?: string;
 };
 
-const Button = ({ children, onClick }: ButtonType) => {
+const Button = ({ children, onClick, className }: ButtonType) => {
   const { resolvedTheme } = useTheme();
 
   return (
     <motion.button
-      className="px-6 py-2 bg-accent-theme-2 text-white rounded-lg cursor-pointer"
+      className={
+        "px-6 py-2 bg-accent-theme-2 text-white rounded-lg cursor-pointer" +
+        className
+      }
       whileHover={{ opacity: 0.85, backgroundColor: "var(--accent-color-1)" }}
       onClick={onClick}
       key={resolvedTheme}
