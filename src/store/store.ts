de95c8ea31bd54896490d5208579bc123b6b14fd
Kretostan <kubakret82@icloud.com>
2025-08-29 12:01:04 +0200

    fix: remove unnecessary checking user info

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,40 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
-import axios from "axios";
 
 import menuReducer from "../store/menuSlice";
 import authReducer from "../store/authSlice";
 import modalReducer from "../store/modalSlice.ts";
-
-const fetchUserInfo = async () => {
-  try {
-    const response = await axios.get(import.meta.env.VITE_API_URL + "/user", {
-      withCredentials: true,
-    });
-    return response.data;
-  } catch (error) {
-    console.error("Failed to fetch user info:", error);
-    return { token: null, role: null };
-  }
-};
-
-const loadInitialAuthState = async () => {
-  const userInfo = await fetchUserInfo();
-  return {
-    token: userInfo.token,
-    role: userInfo.role,
-  };
-};
-
-const initialState = await loadInitialAuthState();
 
 export const store = configureStore({
   reducer: {
     menu: menuReducer,
     auth: authReducer,
     modal: modalReducer,
-  },
-  preloadedState: {
-    auth: initialState,
   },
 });
 
