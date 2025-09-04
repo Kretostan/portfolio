75c4ee7305dec0ec8647fc4bc418bca651466206
Kretostan <kubakret82@icloud.com>
2025-09-04 22:38:40 +0200

    style: add hover animation

--- src/components/Projects/BackButton.tsx
+++ src/components/Projects/BackButton.tsx
@@ -1,15 +1,23 @@
 import { useNavigate } from "react-router";
+import { useTheme } from "next-themes";
+import { motion } from "framer-motion";
 
 const BackButton = () => {
   const navigate = useNavigate();
+  const { resolvedTheme } = useTheme();
 
   return (
-    <button
+    <motion.button
+      whileHover={{
+        backgroundColor: "var(--foreground-1)",
+        color: "var(--background-1)",
+      }}
       onClick={() => navigate("/projects")}
-      className="cursor-pointer px-3 py-1 border-1 rounded"
+      className="cursor-pointer px-4 py-1 border-1 rounded-lg text-lg"
+      key={resolvedTheme}
     >
-      Go Back
-    </button>
+      Back
+    </motion.button>
   );
 };
 
