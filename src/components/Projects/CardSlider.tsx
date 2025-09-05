083a47e0e620ab70f6a16b3ac702a97b52778f71
Kretostan <kubakret82@icloud.com>
2025-09-05 21:32:29 +0200

    refactor: cleanup up hard coded cards

--- src/components/Projects/CardSlider.tsx
+++ src/components/Projects/CardSlider.tsx
@@ -21,65 +21,6 @@ type CardSliderType = {
 
 type Lang = "pl" | "en";
 
-const cards = [
-  {
-    id: 1,
-    title: "Opis",
-    titleEn: "Description",
-    text: [
-      "Prosta i intuicyjna aplikacja pogodowa zbudowana w architekturze full-stack. Umożliwia sprawdzenie aktualnej pogody w dowolnej lokalizacji oraz prostą zmianę jednostek temperatury między Celsjuszem i Fahrenheitem.",
-    ],
-    textEn: [
-      "A modern weather application built with a full-stack architecture. It allows users to check real-time weather conditions for any location and easily switch between Celsius and Fahrenheit.",
-    ],
-  },
-  {
-    id: 2,
-    title: "Funkcjonalności",
-    titleEn: "Key Features",
-    text: [
-      "Wyszukiwanie pogody w wybranym mieście",
-      "Wyświetlanie temperatury oraz ikony",
-      "Przełącznik temperatury °C ↔ °F",
-      "Responsywny, przyjazny interfejs",
-      "Płynne animacje z Framer Motion",
-    ],
-    textEn: [
-      "Search for weather in a selected city",
-      "Display of temperature and icon",
-      "Temperature toggle °C ↔ °F",
-      "Responsive, user-friendly interface",
-      "Smooth animations with Framer Motion",
-    ],
-  },
-  {
-    id: 3,
-    title: "Frontend",
-    text: [
-      "React",
-      "React Router",
-      "Tailwind CSS",
-      "Framer Motion",
-      "Axios",
-      "Vite",
-      "TypeScript",
-    ],
-  },
-  {
-    id: 4,
-    title: "Backend",
-    text: [
-      "Node.js",
-      "Express.js",
-      "Firebase Functions",
-      "Firebase Admin",
-      "Axios",
-      "dotenv",
-      "cors",
-    ],
-  },
-];
-
 const CardSlider = ({ description }: CardSliderType) => {
   const [card, setCard] = useState<number>(0);
   const { i18n } = useTranslation();
@@ -111,7 +52,7 @@ const CardSlider = ({ description }: CardSliderType) => {
               exit={{ opacity: 0, x: -100, scale: 0.9 }}
               transition={{ duration: 0.3 }}
               className="absolute top-0 left-0 flex flex-col gap-6 px-6 py-4 min-h-[375px] w-80 rounded-2xl border-1 shadow-2xl bg-bg-content"
-              style={{ zIndex: cards.length - index }}
+              style={{ zIndex: description.length - index }}
             >
               <h3 className="pb-3 text-lg sm:text-1xl lg:text-2xl text-center font-semibold border-b-2 border-accent-theme-2">
                 {card.title[lang]}
