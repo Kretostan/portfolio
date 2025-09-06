7491195fcdfae854c47e26de19ced9ae6b6b6892
Kretostan <kubakret82@icloud.com>
2025-09-06 12:47:42 +0200

    fix: type Project has always four descriptions

--- src/@types/Project.ts
+++ src/@types/Project.ts
@@ -1,19 +1,19 @@
+type Description = {
+  id: number;
+  title: {
+    pl: string;
+    en: string;
+  };
+  text: {
+    pl: string[];
+    en: string[];
+  };
+};
+
 export type Project = {
   title: string;
   slug: string;
-  description: [
-    {
-      id: number;
-      title: {
-        pl: string;
-        en: string;
-      };
-      text: {
-        pl: string[];
-        en: string[];
-      };
-    },
-  ];
+  description: [Description, Description, Description, Description];
   image: string;
   url: string;
   github: string;
