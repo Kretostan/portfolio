e63d8096746427fee7c6eb3cbaa954f7a6cdef8d
Kretostan <kubakret82@icloud.com>
2025-09-20 17:27:19 +0200

    style: change font size

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -11,7 +11,7 @@ const AboutPage = () => {
     <>
       <Title>{t("about.title")}</Title>
       <div className="flex flex-col justify-center items-center gap-6 sm:gap-4 py-8 sm:py-16">
-        <p className="max-w-[400px] sm:max-w-full text-center">{t("about.subtitle")}</p>
+        <p className="max-w-[400px] sm:max-w-full text-center md:text-lg">{t("about.subtitle")}</p>
         <div className="flex flex-col lg:flex-row justify-center items-center gap-14 sm:gap-20 py-10 w-full rounded-xl">
           <AboutText />
           <AboutImage />
