b46bf53e71d686a0c7dd64c695b026a856603224
Kretostan <kubakret82@icloud.com>
2025-09-20 16:52:46 +0200

    feat: new Creator and Updater due to projects in Polish and English version

--- src/components/Projects/Updater.tsx
+++ src/components/Projects/Updater.tsx
@@ -1,15 +1,23 @@
 import { type ChangeEvent, type FormEvent, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router";
+import { useTranslation } from "react-i18next";
 
 import type { Project } from "../../@types";
 import Button from "../UI/Button.tsx";
 
+type Lang = "pl" | "en";
+
 const Updater = ({ project }: { project: Project }) => {
   const [formData, setFormData] = useState<Project>(project);
   const navigate = useNavigate();
+  const [descLang, setDescLang] = useState<"en" | "pl">("en");
+  const { i18n } = useTranslation();
+  const lang = i18n.language as Lang;
 
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (
+    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
     const { name, value } = event.target;
     setFormData((prevProject) => {
       return {
@@ -21,7 +29,6 @@ const Updater = ({ project }: { project: Project }) => {
 
   const onSubmitUpdate = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log(formData);
     const response = await axios.patch(
       import.meta.env.VITE_API_URL + "/projects/" + project.slug,
       formData,
@@ -41,67 +48,289 @@ const Updater = ({ project }: { project: Project }) => {
       onSubmit={onSubmitUpdate}
       className="px-8 py-6 max-w-[500px] bg-bg-theme-2 rounded-xl"
     >
-      <div>
-        <div className="flex flex-col py-2 text-sm">
-          <label id="title" htmlFor="title" className="py-1">
-            Title:
-          </label>
-          <input
-            type="text"
-            name="title"
-            placeholder="Title..."
-            value={formData.title}
-            onChange={handleChange}
-            required
-          />
+      <div className="flex flex-col gap-4">
+        <div>
+          <div>
+            <label htmlFor="title" className="text-error-label-color">
+              Project Title
+            </label>
+            <input
+              type="text"
+              id="title"
+              name="title"
+              placeholder="Title"
+              value={formData.title}
+              onChange={handleChange}
+              className="bg-error-bg-color border-1 border-error-label-color placeholder:text-error-color"
+            />
+          </div>
+          <div>
+            <label htmlFor="image">Project Image</label>
+            <input
+              type="text"
+              id="image"
+              name="image"
+              value={formData.image}
+              onChange={handleChange}
+              placeholder="AWS S3"
+            />
+          </div>
+          <div>
+            <label htmlFor="url">Project URL</label>
+            <input
+              type="text"
+              id="url"
+              name="url"
+              value={formData.url}
+              onChange={handleChange}
+              placeholder="Page URL"
+            />
+          </div>
+          <div>
+            <label htmlFor="github">Project Repo</label>
+            <input
+              type="text"
+              id="github"
+              name="github"
+              value={formData.github}
+              onChange={handleChange}
+              placeholder="Repo"
+            />
+          </div>
         </div>
-        <div className="flex flex-col py-2 text-sm">
-          <label id="description" htmlFor="description" className="py-1">
-            Description:
-          </label>
-          <input
-            type="text"
-            name="description"
-            placeholder="Description..."
-            value={formData.description}
-            required
-          />
-        </div>
-        <div className="flex flex-col py-2 text-sm">
-          <label id="image" htmlFor="image" className="py-1">
-            Image:
-          </label>
-          <input
-            type="text"
-            name="image"
-            placeholder="Image..."
-            value={formData.image}
-            required
-          />
-        </div>
-        <div className="flex flex-col py-2 text-sm">
-          <label id="url" htmlFor="url" className="py-1">
-            URL:
-          </label>
-          <input
-            type="text"
-            name="url"
-            placeholder="URL..."
-            value={formData.url}
-            required
-          />
-        </div>
-        <div className="flex flex-col py-2 text-sm">
-          <label id="github" htmlFor="github" className="py-1">
-            GitHub:
-          </label>
-          <input
-            type="text"
-            name="github"
-            placeholder="GitHub..."
-            value={formData.github}
-            required
-          />
+        <div>
+          <div className="relative w-[300px] h-[225px]">
+            <div>
+              <h3 className="font-semibold text-center pb-4">
+                Description ({descLang.toUpperCase()})
+              </h3>
+              <button
+                type="button"
+                onClick={() =>
+                  setDescLang((prev) => (prev === "pl" ? "en" : "pl"))
+                }
+                className="absolute top-0 right-0 text-lg cursor-pointer"
+              >
+                ✅
+              </button>
+            </div>
+            <div
+              className={`absolute top-10 left-0 ${descLang === "en" ? "visible" : "hidden"}`}
+            >
+              <div>
+                <div>
+                  <label htmlFor={`firstDescriptionTitleEN`}>
+                    Description Title
+                  </label>
+                  <input
+                    id={`firstDescriptionTitleEN`}
+                    name={`firstDescriptionTitleEN`}
+                    value={formData.description[0].title[lang]}
+                    onChange={handleChange}
+                    placeholder={`First Title (EN)`}
+                  ></input>
+                </div>
+                <div>
+                  <label htmlFor={`firstDescriptionTextEN`}>
+                    Description Text
+                  </label>
+                  <textarea
+                    id={`firstDescriptionTextEN`}
+                    name={`firstDescriptionTextEN`}
+                    value={formData.description[0].text[lang]}
+                    onChange={handleChange}
+                    placeholder={`First Text (EN)`}
+                  ></textarea>
+                </div>
+              </div>
+              <div>
+                <div>
+                  <label htmlFor={`secondDescriptionTitleEN`}>
+                    Description Title
+                  </label>
+                  <input
+                    id={`secondDescriptionTitleEN`}
+                    name={`secondDescriptionTitleEN`}
+                    value={formData.description[1].title[lang]}
+                    onChange={handleChange}
+                    placeholder={`Second Title (EN)`}
+                  ></input>
+                </div>
+                <div>
+                  <label htmlFor={`secondDescriptionTextEN`}>
+                    Description Text
+                  </label>
+                  <textarea
+                    id={`secondDescriptionTextEN`}
+                    name={`secondDescriptionTextEN`}
+                    value={formData.description[1].text[lang]}
+                    onChange={handleChange}
+                    placeholder={`Second Text (EN)`}
+                  ></textarea>
+                </div>
+              </div>
+              <div>
+                <div>
+                  <label htmlFor={`thirdDescriptionTitleEN`}>
+                    Description Title
+                  </label>
+                  <input
+                    id={`thirdDescriptionTitleEN`}
+                    name={`thirdDescriptionTitleEN`}
+                    value={formData.description[2].title[lang]}
+                    onChange={handleChange}
+                    placeholder={`Third Title (EN)`}
+                  ></input>
+                </div>
+                <div>
+                  <label htmlFor={`thirdDescriptionTextEN`}>
+                    Description Text
+                  </label>
+                  <textarea
+                    id={`thirdDescriptionTextEN`}
+                    name={`thirdDescriptionTextEN`}
+                    value={formData.description[2].text[lang]}
+                    onChange={handleChange}
+                    placeholder={`Third Text (EN)`}
+                  ></textarea>
+                </div>
+              </div>
+              <div>
+                <div>
+                  <label htmlFor={`fourthDescriptionTitleEN`}>
+                    Description Title
+                  </label>
+                  <input
+                    id={`fourthDescriptionTitleEN`}
+                    name={`fourthDescriptionTitleEN`}
+                    value={formData.description[3].title[lang]}
+                    onChange={handleChange}
+                    placeholder={`Fourth Title (EN)`}
+                  ></input>
+                </div>
+                <div>
+                  <label htmlFor={`fourthDescriptionTextEN`}>
+                    Description Text
+                  </label>
+                  <textarea
+                    id={`fourthDescriptionTextEN`}
+                    name={`fourthDescriptionTextEN`}
+                    value={formData.description[3].text[lang]}
+                    onChange={handleChange}
+                    placeholder={`Fourth Text (EN)`}
+                  ></textarea>
+                </div>
+              </div>
+            </div>
+            <div
+              className={`absolute top-10 left-0 ${descLang === "pl" ? "visible" : "hidden"}`}
+            >
+              <div>
+                <div>
+                  <label
+                    htmlFor={`firstDescriptionTitle${descLang.toUpperCase()}`}
+                  >
+                    Description Title
+                  </label>
+                  <textarea
+                    id={`firstDescriptionTitle${descLang.toUpperCase()}`}
+                    name={`firstDescriptionTitle${descLang.toUpperCase()}`}
+                    placeholder={`Title (${descLang.toUpperCase()})`}
+                  ></textarea>
+                </div>
+                <div>
+                  <label
+                    htmlFor={`firstDescriptionText${descLang.toUpperCase()}`}
+                  >
+                    Description Text
+                  </label>
+                  <textarea
+                    id={`firstDescriptionText${descLang.toUpperCase()}`}
+                    name={`firstDescriptionText${descLang.toUpperCase()}`}
+                    placeholder={`Text (${descLang.toUpperCase()})`}
+                  ></textarea>
+                </div>
+              </div>
+              <div>
+                <div>
+                  <label
+                    htmlFor={`secondDescriptionTitle${descLang.toUpperCase()}`}
+                  >
+                    Description Title
+                  </label>
+                  <textarea
+                    id={`secondDescriptionTitle${descLang.toUpperCase()}`}
+                    name={`secondDescriptionTitle${descLang.toUpperCase()}`}
+                    placeholder={`Title (${descLang.toUpperCase()})`}
+                  ></textarea>
+                </div>
+                <div>
+                  <label
+                    htmlFor={`secondDescriptionText${descLang.toUpperCase()}`}
+                  >
+                    Description Text
+                  </label>
+                  <textarea
+                    id={`secondDescriptionText${descLang.toUpperCase()}`}
+                    name={`secondDescriptionText${descLang.toUpperCase()}`}
+                    placeholder={`Text (${descLang.toUpperCase()})`}
+                  ></textarea>
+                </div>
+              </div>
+              <div>
+                <div>
+                  <label
+                    htmlFor={`thirdDescriptionTitle${descLang.toUpperCase()}`}
+                  >
+                    Description Title
+                  </label>
+                  <textarea
+                    id={`thirdDescriptionTitle${descLang.toUpperCase()}`}
+                    name={`thirdDescriptionTitle${descLang.toUpperCase()}`}
+                    placeholder={`Title (${descLang.toUpperCase()})`}
+                  ></textarea>
+                </div>
+                <div>
+                  <label
+                    htmlFor={`thirdDescriptionText${descLang.toUpperCase()}`}
+                  >
+                    Description Text
+                  </label>
+                  <textarea
+                    id={`thirdDescriptionText${descLang.toUpperCase()}`}
+                    name={`thirdDescriptionText${descLang.toUpperCase()}`}
+                    placeholder={`Text (${descLang.toUpperCase()})`}
+                  ></textarea>
+                </div>
+              </div>
+              <div>
+                <div>
+                  <label
+                    htmlFor={`fourthDescriptionTitle${descLang.toUpperCase()}`}
+                  >
+                    Description Title
+                  </label>
+                  <textarea
+                    id={`fourthDescriptionTitle${descLang.toUpperCase()}`}
+                    name={`fourthDescriptionTitle${descLang.toUpperCase()}`}
+                    placeholder={`Title (${descLang.toUpperCase()})`}
+                  ></textarea>
+                </div>
+                <div>
+                  <label
+                    htmlFor={`fourthDescriptionText${descLang.toUpperCase()}`}
+                  >
+                    Description Text
+                  </label>
+                  <textarea
+                    id={`fourthDescriptionText${descLang.toUpperCase()}`}
+                    name={`fourthDescriptionText${descLang.toUpperCase()}`}
+                    placeholder={`Text (${descLang.toUpperCase()})`}
+                  ></textarea>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       <div className="flex justify-end pt-3 w-full">
--- src/pages/projects/Creator.tsx
+++ src/pages/projects/Creator.tsx
@@ -1,15 +1,19 @@
+import { type FormEvent, useState } from "react";
 import axios from "axios";
 import { Navigate, useNavigate } from "react-router";
 import { useSelector } from "react-redux";
 
 import type { RootState } from "../../store/store.ts";
-import type { FormEvent } from "react";
+
 import Title from "../../components/UI/Title.tsx";
 import Button from "../../components/UI/Button.tsx";
 
 const CreatorPage = () => {
   const navigate = useNavigate();
   const auth = useSelector((state: RootState) => state.auth);
+  const [descLang, setDescLang] = useState<"en" | "pl">("en");
+  // TODO: Show desc input based on descIndex
+  // const [descIndex, setDescIndex] = useState<number>(0);
 
   if (!auth.isLoggedIn && auth.role !== "admin") {
     return <Navigate to="/" replace />;
@@ -37,40 +41,252 @@ const CreatorPage = () => {
       <Title>Creator Page</Title>
       <form
         onSubmit={handleAddProject}
-        className="flex flex-col gap-2 mx-3 sm:mx-0 p-8 sm:max-w-[500px] sm:w-full bg-bg-theme-2 rounded-xl ring"
+        className="flex flex-col gap-2 mx-3 sm:mx-0 p-8 bg-bg-theme-2 rounded-xl ring"
       >
         <div className="flex flex-col gap-4">
           <div>
-            <label htmlFor="title" className="text-error-label-color">
-              Project Title
-            </label>
-            <input
-              type="text"
-              id="title"
-              name="title"
-              placeholder="Title"
-              className="bg-error-bg-color border-1 border-error-label-color placeholder:text-error-color"
-            />
+            <div>
+              <label htmlFor="title" className="text-error-label-color">
+                Project Title
+              </label>
+              <input
+                type="text"
+                id="title"
+                name="title"
+                placeholder="Title"
+                className="bg-error-bg-color border-1 border-error-label-color placeholder:text-error-color"
+              />
+            </div>
+            <div>
+              <label htmlFor="image">Project Image</label>
+              <input type="text" id="image" name="image" placeholder="AWS S3" />
+            </div>
+            <div>
+              <label htmlFor="url">Project URL</label>
+              <input type="text" id="url" name="url" placeholder="Page URL" />
+            </div>
+            <div>
+              <label htmlFor="github">Project Repo</label>
+              <input type="text" id="github" name="github" placeholder="Repo" />
+            </div>
           </div>
           <div>
-            <label htmlFor="image">Project Image</label>
-            <input type="text" id="image" name="image" placeholder="AWS S3" />
-          </div>
-          <div>
-            <label htmlFor="url">Project URL</label>
-            <input type="text" id="url" name="url" placeholder="Page URL" />
-          </div>
-          <div>
-            <label htmlFor="github">Project Repo</label>
-            <input type="text" id="github" name="github" placeholder="Repo" />
-          </div>
-          <div>
-            <label htmlFor="description">Project Description</label>
-            <textarea
-              id="description"
-              name="description"
-              placeholder="Description"
-            ></textarea>
+            <div className="relative w-[300px]">
+              <div>
+                <h3 className="font-semibold text-center pb-4">
+                  Description ({descLang.toUpperCase()})
+                </h3>
+                <button
+                  type="button"
+                  onClick={() =>
+                    setDescLang((prev) => (prev === "pl" ? "en" : "pl"))
+                  }
+                  className="absolute top-0 right-0 text-lg cursor-pointer"
+                >
+                  ✅
+                </button>
+              </div>
+              <div
+                className={`absolute top-10 left-0 ${descLang === "en" ? "visible" : "hidden"}`}
+              >
+                <div>
+                  <div>
+                    <label htmlFor={`firstDescriptionTitleEN`}>
+                      Description Title
+                    </label>
+                    <textarea
+                      id={`firstDescriptionTitleEN`}
+                      name={`firstDescriptionTitleEN`}
+                      placeholder={`First Title (EN)`}
+                    ></textarea>
+                  </div>
+                  <div>
+                    <label htmlFor={`firstDescriptionTextEN`}>
+                      Description Text
+                    </label>
+                    <textarea
+                      id={`firstDescriptionTextEN`}
+                      name={`firstDescriptionTextEN`}
+                      placeholder={`First Text (EN)`}
+                    ></textarea>
+                  </div>
+                </div>
+                <div>
+                  <div>
+                    <label htmlFor={`secondDescriptionTitleEN`}>
+                      Description Title
+                    </label>
+                    <textarea
+                      id={`secondDescriptionTitleEN`}
+                      name={`secondDescriptionTitleEN`}
+                      placeholder={`Second Title (EN)`}
+                    ></textarea>
+                  </div>
+                  <div>
+                    <label htmlFor={`secondDescriptionTextEN`}>
+                      Description Text
+                    </label>
+                    <textarea
+                      id={`secondDescriptionTextEN`}
+                      name={`secondDescriptionTextEN`}
+                      placeholder={`Second Text (EN)`}
+                    ></textarea>
+                  </div>
+                </div>
+                <div>
+                  <div>
+                    <label htmlFor={`thirdDescriptionTitleEN`}>
+                      Description Title
+                    </label>
+                    <textarea
+                      id={`thirdDescriptionTitleEN`}
+                      name={`thirdDescriptionTitleEN`}
+                      placeholder={`Third Title (EN)`}
+                    ></textarea>
+                  </div>
+                  <div>
+                    <label htmlFor={`thirdDescriptionTextEN`}>
+                      Description Text
+                    </label>
+                    <textarea
+                      id={`thirdDescriptionTextEN`}
+                      name={`thirdDescriptionTextEN`}
+                      placeholder={`Third Text (EN)`}
+                    ></textarea>
+                  </div>
+                </div>
+                <div>
+                  <div>
+                    <label htmlFor={`fourthDescriptionTitleEN`}>
+                      Description Title
+                    </label>
+                    <textarea
+                      id={`fourthDescriptionTitleEN`}
+                      name={`fourthDescriptionTitleEN`}
+                      placeholder={`Fourth Title (EN)`}
+                    ></textarea>
+                  </div>
+                  <div>
+                    <label htmlFor={`fourthDescriptionTextEN`}>
+                      Description Text
+                    </label>
+                    <textarea
+                      id={`fourthDescriptionTextEN`}
+                      name={`fourthDescriptionTextEN`}
+                      placeholder={`Fourth Text (EN)`}
+                    ></textarea>
+                  </div>
+                </div>
+              </div>
+              <div
+                className={`absolute top-10 left-0 ${descLang === "pl" ? "visible" : "hidden"}`}
+              >
+                <div>
+                  <div>
+                    <label
+                      htmlFor={`firstDescriptionTitle${descLang.toUpperCase()}`}
+                    >
+                      Description Title
+                    </label>
+                    <textarea
+                      id={`firstDescriptionTitle${descLang.toUpperCase()}`}
+                      name={`firstDescriptionTitle${descLang.toUpperCase()}`}
+                      placeholder={`Title (${descLang.toUpperCase()})`}
+                    ></textarea>
+                  </div>
+                  <div>
+                    <label
+                      htmlFor={`firstDescriptionText${descLang.toUpperCase()}`}
+                    >
+                      Description Text
+                    </label>
+                    <textarea
+                      id={`firstDescriptionText${descLang.toUpperCase()}`}
+                      name={`firstDescriptionText${descLang.toUpperCase()}`}
+                      placeholder={`Text (${descLang.toUpperCase()})`}
+                    ></textarea>
+                  </div>
+                </div>
+                <div>
+                  <div>
+                    <label
+                      htmlFor={`secondDescriptionTitle${descLang.toUpperCase()}`}
+                    >
+                      Description Title
+                    </label>
+                    <textarea
+                      id={`secondDescriptionTitle${descLang.toUpperCase()}`}
+                      name={`secondDescriptionTitle${descLang.toUpperCase()}`}
+                      placeholder={`Title (${descLang.toUpperCase()})`}
+                    ></textarea>
+                  </div>
+                  <div>
+                    <label
+                      htmlFor={`secondDescriptionText${descLang.toUpperCase()}`}
+                    >
+                      Description Text
+                    </label>
+                    <textarea
+                      id={`secondDescriptionText${descLang.toUpperCase()}`}
+                      name={`secondDescriptionText${descLang.toUpperCase()}`}
+                      placeholder={`Text (${descLang.toUpperCase()})`}
+                    ></textarea>
+                  </div>
+                </div>
+                <div>
+                  <div>
+                    <label
+                      htmlFor={`thirdDescriptionTitle${descLang.toUpperCase()}`}
+                    >
+                      Description Title
+                    </label>
+                    <textarea
+                      id={`thirdDescriptionTitle${descLang.toUpperCase()}`}
+                      name={`thirdDescriptionTitle${descLang.toUpperCase()}`}
+                      placeholder={`Title (${descLang.toUpperCase()})`}
+                    ></textarea>
+                  </div>
+                  <div>
+                    <label
+                      htmlFor={`thirdDescriptionText${descLang.toUpperCase()}`}
+                    >
+                      Description Text
+                    </label>
+                    <textarea
+                      id={`thirdDescriptionText${descLang.toUpperCase()}`}
+                      name={`thirdDescriptionText${descLang.toUpperCase()}`}
+                      placeholder={`Text (${descLang.toUpperCase()})`}
+                    ></textarea>
+                  </div>
+                </div>
+                <div>
+                  <div>
+                    <label
+                      htmlFor={`fourthDescriptionTitle${descLang.toUpperCase()}`}
+                    >
+                      Description Title
+                    </label>
+                    <textarea
+                      id={`fourthDescriptionTitle${descLang.toUpperCase()}`}
+                      name={`fourthDescriptionTitle${descLang.toUpperCase()}`}
+                      placeholder={`Title (${descLang.toUpperCase()})`}
+                    ></textarea>
+                  </div>
+                  <div>
+                    <label
+                      htmlFor={`fourthDescriptionText${descLang.toUpperCase()}`}
+                    >
+                      Description Text
+                    </label>
+                    <textarea
+                      id={`fourthDescriptionText${descLang.toUpperCase()}`}
+                      name={`fourthDescriptionText${descLang.toUpperCase()}`}
+                      placeholder={`Text (${descLang.toUpperCase()})`}
+                    ></textarea>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         <div className="flex flex-col gap-2">
