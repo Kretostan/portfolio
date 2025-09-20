9a766f402eff762f44b82f72cc1c05a0b2b365e5
Kretostan <kubakret82@icloud.com>
2025-09-20 16:34:32 +0200

    style: change position of tool tip

--- src/components/UI/Tooltip.tsx
+++ src/components/UI/Tooltip.tsx
@@ -6,7 +6,7 @@ const Tooltip = ({ name }: { name: string }) => {
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
-      className="absolute -top-20 -left-6 flex justify-center items-center px-4 py-2 w-[8em] bg-bg-theme-1 rounded-lg text-center ring-1"
+      className="absolute -top-1/2 left-1/2 -translate-x-1/2 flex justify-center items-center px-4 py-2 w-[8em] bg-bg-theme-1 rounded-lg text-center font-semibold border-2 text-lg border-accent-theme-1"
     >
       {name}
     </motion.div>
