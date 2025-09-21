f8d055af0b04329a4ee19c2857c339e72eca4ba4
Kretostan <kubakret82@icloud.com>
2025-09-21 23:02:21 +0200

    feat: add Protected Route wrapper

--- src/pages/projects/Updater.tsx
+++ src/pages/projects/Updater.tsx
@@ -9,59 +9,58 @@ import Spinner from "../../components/UI/Spinner.tsx";
 
 import type { Project } from "../../@types";
 import type { RootState } from "../../store/store.ts";
+import ProtectedRoute from "../../components/ProtectedRoute.tsx";
 
 const ProjectUpdaterPage = () => {
   const auth = useSelector((state: RootState) => state.auth);
   const navigate = useNavigate();
   const { project } = useLoaderData<{ project: Project }>();
 
-  if (!auth.isLoggedIn && auth.role !== "admin") {
-    return <Navigate to="/" replace />;
-  }
-
   return (
-    <div className="flex flex-col gap-12">
-      <Title>Update Project</Title>
-      <div className="flex flex-col gap-3 px-4 w-full">
-        <div className="flex justify-between px-4">
-          <button
-            onClick={() => navigate("/projects")}
-            className="cursor-pointer px-2 py-1 border-1 rounded"
-          >
-            Back
-          </button>
-          {auth.role === "admin" && (
-            <div className="flex gap-6">
-              <button
-                className="bg-accent-theme-1 text-bg-theme-2 px-4 py-2 rounded cursor-pointer"
-                onClick={async () => {
-                  try {
-                    const response = await axios.delete(
-                      import.meta.env.VITE_API_URL +
+    <ProtectedRoute>
+      <div className="flex flex-col gap-12">
+        <Title>Update Project</Title>
+        <div className="flex flex-col gap-3 px-4 w-full">
+          <div className="flex justify-between px-4">
+            <button
+              onClick={() => navigate("/projects")}
+              className="cursor-pointer px-2 py-1 border-1 rounded"
+            >
+              Back
+            </button>
+            {auth.role === "admin" && (
+              <div className="flex gap-6">
+                <button
+                  className="bg-accent-theme-1 text-bg-theme-2 px-4 py-2 rounded cursor-pointer"
+                  onClick={async () => {
+                    try {
+                      const response = await axios.delete(
+                        import.meta.env.VITE_API_URL +
                         "/projects/" +
                         project.slug,
-                      { withCredentials: true },
-                    );
-                    if (response.status === 200) {
-                      navigate("/projects");
+                        { withCredentials: true },
+                      );
+                      if (response.status === 200) {
+                        navigate("/projects");
+                      }
+                    } catch (error) {
+                      console.error("Error deleting project:", error);
                     }
-                  } catch (error) {
-                    console.error("Error deleting project:", error);
-                  }
-                }}
-              >
-                Delete
-              </button>
-            </div>
-          )}
+                  }}
+                >
+                  Delete
+                </button>
+              </div>
+            )}
+          </div>
+          <Suspense fallback={<Spinner />}>
+            <Await resolve={project}>
+              {(resolvedProject) => <Updater project={resolvedProject} />}
+            </Await>
+          </Suspense>
         </div>
-        <Suspense fallback={<Spinner />}>
-          <Await resolve={project}>
-            {(resolvedProject) => <Updater project={resolvedProject} />}
-          </Await>
-        </Suspense>
       </div>
-    </div>
+    </ProtectedRoute>
   );
 };
 
