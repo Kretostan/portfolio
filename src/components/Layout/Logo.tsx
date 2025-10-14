2155e900369f1019437346f3185452a8eea01fd4
Kretostan <kubakret82@icloud.com>
2025-10-14 22:40:51 +0200

    refactor: remove redirecting to home page on click

--- src/components/Layout/Logo.tsx
+++ src/components/Layout/Logo.tsx
@@ -1,13 +1,6 @@
-import { useNavigate } from "react-router";
-
 const Logo = () => {
-  const navigate = useNavigate();
-
   return (
-    <button
-      className="flex items-center px-3 py-3 sm:py-4 cursor-pointer"
-      onClick={() => navigate("/")}
-    >
+    <button className="flex items-center px-3 py-3 sm:py-4 cursor-pointer">
       <img src="/kretostan-logo.png" alt="Kretostan logo" width={170} draggable={false} />
     </button>
   );
