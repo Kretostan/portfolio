e646c3aa322a0a9fd2d9d2bae75becccc125b677
Kretostan <kubakret82@icloud.com>
2025-10-14 19:16:32 +0200

    fix: render empty string of there is no content or category

--- src/components/Skills/SkillsContainer.tsx
+++ src/components/Skills/SkillsContainer.tsx
@@ -23,8 +23,8 @@ const SkillsContainer = ({ skills }: SkillsProps) => {
       className="flex flex-wrap justify-center items-center gap-6 px-2 pt-12 pb-2"
     >
       <motion.div whileHover={{ y: -5, boxShadow: "0 0 10px 2px var(--accent-color-1)", backgroundColor: "var(--background-1)", outlineColor: "var(--accent-color-2)" }} className="flex flex-col gap-4 p-6 h-[12em] w-[20em] outline-2 outline-accent-theme-1 bg-bg-content rounded-xl cursor-pointer" key={resolvedTheme}>
-        <h4 className="pt-2 text-accent-theme-1 font-bold text-xl ">{skill.category[language]}</h4>
-        <p>{skill.content[language]}</p>
+        <h4 className="pt-2 text-accent-theme-1 font-bold text-xl ">{skill.category?.[language] ?? "-"}</h4>
+        <p>{skill.content?.[language] ?? ""}</p>
       </motion.div>
     </motion.div>
   </AnimatePresence>);
