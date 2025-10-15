ac52e817d7cc8b855439a1e4020a3877384eec96
Kretostan <kubakret82@icloud.com>
2025-10-15 09:45:12 +0200

    style: remove pointer cursor

--- src/components/Layout/Logo.tsx
+++ src/components/Layout/Logo.tsx
@@ -1,6 +1,6 @@
 const Logo = () => {
   return (
-    <button className="flex items-center px-3 py-3 sm:py-4 cursor-pointer">
+    <button className="flex items-center px-3 py-3 sm:py-4">
       <img src="/kretostan-logo.png" alt="Kretostan logo" width={170} draggable={false} />
     </button>
   );
