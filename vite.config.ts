8bcd882392e43805b1ff8709856b36e417ba04ba
Kretostan <kubakret82@icloud.com>
2025-10-15 11:10:08 +0200

    build: install Vite plugin to transform SVGs into React components

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tailwindcss from '@tailwindcss/vite';
+import reactsvgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    reactsvgr(),
     react(),
     tailwindcss(),
   ],
