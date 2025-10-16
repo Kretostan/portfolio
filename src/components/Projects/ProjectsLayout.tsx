f4623c51459c43563a550e5df97db287b6db0fc5
Kretostan <kubakret82@icloud.com>
2025-10-16 10:05:07 +0200

    style: add gradient on text header

--- src/components/Projects/ProjectsLayout.tsx
+++ src/components/Projects/ProjectsLayout.tsx
@@ -11,7 +11,7 @@ const ProjectsLayout = () => {
 
   return (
     <section id="projects" className="pt-18">
-      <h3 className="flex flex-col md:flex-row justify-center items-center gap-2 md:gap-4 lg:gap-6 text-4xl font-black text-accent-theme-2 font-header">{t("projects.title")}</h3>
+      <h3 className="flex flex-col md:flex-row justify-center items-center gap-2 md:gap-4 lg:gap-6 text-4xl font-black text-transparent bg-clip-text bg-linear-to-r from-accent-theme-1 to-accent-theme-2 font-header">{t("projects.title")}</h3>
       <div className="flex flex-col justify-center items-center gap-6 pt-8">
         <Suspense fallback={<Spinner />}>
           <Await resolve={projects}>
