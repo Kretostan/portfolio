5550026c0807c6c365196bc864e99eefcfa5a95c
Kretostan <kubakret82@icloud.com>
2025-10-17 17:11:40 +0200

    refactor: change skills header

--- src/components/Skills/SkillsList.tsx
+++ src/components/Skills/SkillsList.tsx
@@ -21,9 +21,9 @@ const SkillsList = ({ skills }: SkillsProps) => {
       className="flex flex-wrap justify-center items-center gap-6 md:gap-12 px-2 pb-2"
     >
       <motion.div whileHover={{ y: -5, boxShadow: "0 0 10px 2px var(--accent-color-1)", backgroundColor: "var(--background-1)", outlineColor: "var(--accent-color-2)" }} className="flex flex-col gap-4 p-6 h-[12em] w-[20em] outline-2 outline-accent-theme-1 bg-bg-content rounded-xl cursor-pointer" key={resolvedTheme}>
-        <h4 className="pt-2 text-accent-theme-1 font-bold text-xl ">
+        <h3 className="pt-2 text-accent-theme-1 font-bold text-xl ">
           {skill.category?.[language] ?? skill.category?.en ?? "—"}
-        </h4>
+        </h3>
         <p>{skill.content?.[language] ?? skill.content?.en ?? ""}</p>
       </motion.div>
     </motion.div>
