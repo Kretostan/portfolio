c0cbce0bd0ea2e54ad4f489423efb6ba28be72dc
Kretostan <kubakret82@icloud.com>
2025-10-27 15:52:15 +0100

    feat: fetch data spinner component

---
+++ src/components/UI/Spinner.tsx
@@ -0,0 +1,9 @@
+const Spinner = ({ content }: { content: string }) => {
+  return <div className="flex flex-col justify-center items-center gap-6">
+    <p>Loading {content}...</p>
+    <div className="h-12 w-12 spinner border-5 border-t-accent-theme-2 border-b-accent-theme-2 border-l-transparent border-r-transparent rounded-full animate-spin">
+    </div>
+  </div>;
+}
+
+export default Spinner;
