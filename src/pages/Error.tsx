cab9f7b5a3f30ee9493d20039ea4c9863138fc59
Kretostan <kubakret82@icloud.com>
2025-10-27 16:17:23 +0100

    style: improve styles

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,16 +1,22 @@
 import { useNavigate } from "react-router";
+import {motion} from "framer-motion";
 
 const ErrorPage = () => {
 	const navigate = useNavigate();
 
 	return (
-		<div className="flex w-screen flex-col items-center justify-center gap-y-4 pt-36 text-xl">
-			<h2 className="pb-10 text-5xl font-bold"><span>404</span> Not Found</h2>
+		<div className="flex w-screen flex-col items-center gap-4 pt-36 h-screen bg-bg-theme-1 text-xl">
+			<h2 className="pb-10 text-5xl font-bold"><span className="bg-clip-text text-transparent bg-linear-to-r from-accent-theme-1 to-accent-theme-2">404</span> Not Found</h2>
 			<p>Could not find requested resource</p>
-			<button
+			<motion.button
+				whileHover={{
+					color: "var(--accent-color-1)"
+				}}
 				onClick={() => navigate("/")}
 				className="cursor-pointer"
-			>Home Page</button>
+			>
+				Home Page
+			</motion.button>
 		</div>
 	);
 }
